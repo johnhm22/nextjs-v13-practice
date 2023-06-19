57d2a9add79030fbbb1600863842d81eb876504e
john <johnhowardmorgan@outlook.com>
2023-06-19 14:54:10 +0100

    Correcting Vercel deployment error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,25 @@
 import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
-// import GoogleProvider from 'next-auth/providers/Google';
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from '@utils/database';
 import User from '@models/user';
 
-// type session =
+interface INewProfile {
+    iss: string;
+    azp: string;
+    aud: string;
+    sub: string;
+    email: string;
+    email_verified: boolean;
+    at_hash: string;
+    name: string;
+    picture: string;
+    given_name: string;
+    locale: string;
+    iat: number;
+    exp: number;
+}
 
 const handler: NextAuthOptions = NextAuth({
     providers: [
@@ -26,12 +39,15 @@ const handler: NextAuthOptions = NextAuth({
         },
 
         async signIn({ profile }) {
+            console.log('profile: ', profile);
             try {
                 await connectToDB();
                 //check if a user already exists
                 const userExists = await User.findOne({
                     email: profile!.email,
                 });
+
+                console.log('userExists: ', userExists);
                 //if not, create a new user and save to the database
                 if (!userExists && profile) {
                     await User.create({
