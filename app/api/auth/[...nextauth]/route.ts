50e84f1be9c3d095b7c0d4e3095cfb7bd850d344
john <johnhowardmorgan@outlook.com>
2023-06-28 10:36:49 +0100

    Updating ts issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,9 @@
-import { NextAuthOptions } from 'next-auth';
+import { DefaultSession, NextAuthOptions, Session } from 'next-auth';
 import NextAuth from 'next-auth/next';
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
 
 import { connectToDB } from '@utils/database';
 import User from '@models/user';
-
-interface INewProfile {
-    iss: string;
-    azp: string;
-    aud: string;
-    sub: string;
-    email: string;
-    email_verified: boolean;
-    at_hash: string;
-    name: string;
-    picture: string;
-    given_name: string;
-    locale: string;
-    iat: number;
-    exp: number;
-}
 
 const handler: NextAuthOptions = NextAuth({
     providers: [
@@ -33,27 +17,27 @@ const handler: NextAuthOptions = NextAuth({
             const sessionUser = await User.findOne({
                 email: session.user!.email,
             });
-
             session.user!.id = sessionUser._id.toString();
             return session;
         },
 
         async signIn({ profile }) {
-            console.log('profile: ', profile);
+            // check this out
+            // https://stackoverflow.com/questions/74153445/next-auth-profile-custom-typings
+            // if (!profile) return false;
+            // console.log(profile);
             try {
                 await connectToDB();
                 //check if a user already exists
                 const userExists = await User.findOne({
                     email: profile!.email,
                 });
-
-                console.log('userExists: ', userExists);
                 //if not, create a new user and save to the database
                 if (!userExists && profile) {
                     await User.create({
                         email: profile.email,
                         username: profile!.name!.replace(' ', '').toLowerCase(),
-                        image: profile.picture,
+                        image: (profile as GoogleProfile).picture,
                     });
                 }
                 return true;
